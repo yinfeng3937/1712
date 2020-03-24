a1190be706dca75b03b1e5de4a547e6e69a4fa5a
yinfeng <yinfeng@qq.com>
2020-03-24 10:57:19 +0800

    第一次

---
+++ src/test/java/com/bw/test/Test01.java
@@ -0,0 +1,17 @@
+package com.bw.test;
+
+import org.junit.Test;
+
+public class Test01 {
+
+	@Test
+	public void test01(){
+		
+		System.out.println("aa+bb=cc");
+	}
+	@Test
+	public void test02(){
+		
+		System.out.println("2+2=3");
+	}
+}
